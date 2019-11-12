a9ab146a8a0ac4268d25a022c122ef09d19b055a
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-12 08:14:56 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -45,5 +45,6 @@ def total_even_pairs(src)
   row_index = 0 
   while row_index < src.count do 
     if src[row_index][0]  % 2 == 0 && + src[row_index][1] % 2 == 0 
-      total += src
+      total += src[row_index][0] + src[row_index][1]
+      
 end
